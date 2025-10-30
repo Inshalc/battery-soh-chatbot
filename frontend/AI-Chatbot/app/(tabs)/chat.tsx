2ca467c29c60e65caa086a80a0b0e87cbd36ff34
ayaanahmed05 <ayaan.ahmed@ontariotechu.net>
2025-10-30 15:26:51 -0400

    Added greeting card

--- frontend/AI-Chatbot/app/(tabs)/chat.tsx
+++ frontend/AI-Chatbot/app/(tabs)/chat.tsx
@@ -1,15 +1,22 @@
-import { Text, View } from "react-native";
+import ChatInput from '@/components/chat/ChatInput.js';
+import Screen from '@/components/layout/Screen';
+import { StyleSheet, Text, View } from "react-native";
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function Chat() {
+  const nsets = useSafeAreaInsets();
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/(tabs)/chat.tsx to edit this screen.</Text>
-    </View>
+    <Screen>
+      <View>
+        <Text></Text>
+      </View>
+
+      <ChatInput placeholder='Send a Message...'/>
+    </Screen>
   );
 }
+
+const styles = StyleSheet.create({
+  
+});
