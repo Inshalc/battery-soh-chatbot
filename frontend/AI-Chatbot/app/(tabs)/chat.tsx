42b35ec9df082561fe73976b7a5f7f5ce86de3f0
ayaanahmed05 <ayaan.ahmed@ontariotechu.net>
2025-10-30 18:21:54 -0400

    Styled Chat Page

--- frontend/AI-Chatbot/app/(tabs)/chat.tsx
+++ frontend/AI-Chatbot/app/(tabs)/chat.tsx
@@ -1,22 +1,52 @@
 import ChatInput from '@/components/chat/ChatInput.js';
+import ProfileHeader from '@/components/layout/ProfileHeader';
 import Screen from '@/components/layout/Screen';
-import { StyleSheet, Text, View } from "react-native";
+import { theme } from "@/themes/theme";
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, View } from "react-native";
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
+const INPUT_HEIGHT = 56;
+const tabBarHeight = 52;
+
 export default function Chat() {
-  const nsets = useSafeAreaInsets();
+
+  const insets = useSafeAreaInsets();
+  const bottomOffset = tabBarHeight + insets.bottom;
 
   return (
     <Screen>
+      <ProfileHeader />
+
       <View>
         <Text></Text>
       </View>
-
-      <ChatInput placeholder='Send a Message...'/>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        keyboardVerticalOffset={bottomOffset}
+        style={styles.kav}
+      >
+        <View style={[styles.inputContainer]}>
+          <ChatInput placeholder='Send a Message...' />
+        </View>
+      </KeyboardAvoidingView>
     </Screen>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  kav: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: tabBarHeight,
+  },
+
+  inputContainer: {
+    position: 'absolute',
+    left: 0, right: 0, bottom: 0,
+
+    paddingHorizontal: 20,
+    paddingBottom: theme.spacing.sm
+
+  }
 });
--- frontend/AI-Chatbot/components/layout/ProfileHeader.js
+++ frontend/AI-Chatbot/components/layout/ProfileHeader.js
@@ -2,11 +2,15 @@ import { globalStyles } from '@/themes/globalStyles';
 import { theme } from '@/themes/theme';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import IconButton from '../ui/IconButton';
 
 const ProfileHeader = () => {
+    const insets = useSafeAreaInsets();
+
     return (
-        <View style={styles.container}>
+        <View style={[styles.positioningContainer, {top: insets.top}]}>
+            <View style={styles.container}>
 
             <Text style={globalStyles.title}>Battery SOH Chatbot</Text>
             <View style={styles.iconContainer}>
@@ -14,11 +18,17 @@ const ProfileHeader = () => {
                 <IconButton name="log-out"/>
             </View>
 
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    positioningContainer: {
+        position: 'absolute',
+        left: 0, right: 0, 
+    },
+
     container: {
         display: 'flex',
         flexDirection: 'row',
@@ -26,6 +36,10 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         borderBottomWidth: 1,
         borderBottomColor: theme.colors.border,
+
+        backgroundColor: 'transparent',
+
+        paddingBottom: theme.spacing.sm,
     },
     
     iconContainer: {
